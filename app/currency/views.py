4f9c38031beff02f26c633f2942b9626f9fefb7f
Yevhenii Tsevelov <tsevelov2019@gmail.com>
2021-07-14 17:16:09 +0300

    add Travis ci and new url

--- app/currency/views.py
+++ app/currency/views.py
@@ -1,8 +1,25 @@
-from django.shortcuts import render
+# from django.shortcuts import render
 from django.http import HttpResponse
 
+from currency.models import ContactUs
 # Create your views here.
 
 
 def hello_world(request):
     return HttpResponse('Hello world!')
+
+
+def contactus_list(request):
+    contacts = ContactUs.objects.all()
+
+    result = []
+
+    for contact in contacts:
+        result.append(
+            f'ID: {contact.id} ',
+            f'Email: {contact.email_from} ',
+            f'Subject: {contact.subject} ',
+            f'Message: {contact.message}'
+        )
+
+    return HttpResponse(str(result))
