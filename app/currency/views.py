6144e4ab6d9673985f29298a0cbf2eea67a976da
Yevhenii Tsevelov <tsevelov2019@gmail.com>
2021-07-30 20:47:00 +0300

    removed unnecessary functions

--- app/currency/views.py
+++ app/currency/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, get_object_or_404
-from django.http import HttpResponseRedirect, Http404
+from django.http import HttpResponseRedirect
 
 
 from currency.models import ContactUs, Rate, Source
